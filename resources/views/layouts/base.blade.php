58094bb93260b3752511d2b27f92a60931bf8055
Dries Vints <dries@vints.io>
2020-07-12 20:34:53 +0200

    Favicons

--- resources/views/layouts/base.blade.php
+++ resources/views/layouts/base.blade.php
@@ -6,6 +6,13 @@
 
     <title>{{ config('app.name') }}</title>
 
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('/site.webmanifest') }}">
+    <meta name="msapplication-TileColor" content="#b91d47">
+    <meta name="theme-color" content="#ffffff">
+
     <script src="{{ mix('js/app.js') }}" defer></script>
 
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
