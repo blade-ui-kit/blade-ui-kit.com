5f84512aee526e39687a712c20f1aa315510ef27
Dries Vints <dries@vints.io>
2020-07-26 14:25:43 +0200

    Cleanup

--- resources/views/docs.blade.php
+++ resources/views/docs.blade.php
@@ -1,23 +1,21 @@
 <x-layout>
     <x-navigation />
 
-    <div class="max-w-screen-xl mx-auto px-4 lg:px-6 py-6">
-        <div class="lg:grid lg:grid-cols-5 lg:gap-10">
-            <x-docs.navigation :sections="$sections" :current="$page" :version="$version" />
+    <div class="max-w-screen-xl mx-auto px-4 lg:px-6 py-6 lg:grid lg:grid-cols-5 lg:gap-10">
+        <x-docs.navigation :sections="$sections" :current="$page" :version="$version" />
 
-            <x-markdown anchors class="docs lg:col-span-3 px-3 lg:px-0 mt-6 lg:mt-0 prose lg:prose-lg">
+        <x-markdown anchors class="docs lg:col-span-3 px-3 lg:px-0 mt-8 lg:mt-0 prose lg:prose-lg">
+            {!! $markdown !!}
+        </x-markdown>
+
+        <div class="mt-8">
+            <x-h6>
+                On this page
+            </x-h6>
+
+            <x-toc class="mt-1 toc">
                 {!! $markdown !!}
-            </x-markdown>
-
-            <div class="mt-8">
-                <x-h6>
-                    On this page
-                </x-h6>
-
-                <x-toc class="mt-1 toc">
-                    {!! $markdown !!}
-                </x-toc>
-            </div>
+            </x-toc>
         </div>
     </div>
 
