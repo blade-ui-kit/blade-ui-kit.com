9eddd956780f2913d2d52a30a01763819a8c64b2
Dries Vints <dries@vints.io>
2020-09-02 17:35:03 +0200

    Update repo reference

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -11,7 +11,7 @@ final class WelcomeController
     public function __invoke()
     {
         $downloads = cache()->remember('downloads', 14400, function () {
-            return Http::get('https://packagist.org/packages/blade-ui-kit/blade-icons.json')['package']['downloads']['total'];
+            return Http::get('https://packagist.org/packages/blade-ui-kit/blade-ui-kit.json')['package']['downloads']['total'];
         });
 
         return view('welcome', compact('downloads'));
