06f14c6f66500fca1400610a257ca021f992f1cc
Dries Vints <dries@vints.io>
2020-09-02 17:55:40 +0200

    Require blade ui kit and fix tests

--- tests/Feature/DocsTest.php
+++ tests/Feature/DocsTest.php
@@ -14,11 +14,11 @@ class DocsTest extends TestCase
     {
         $this->partialMock(Filesystem::class, function ($mock) {
             $mock->shouldReceive('exists')->andReturn(true);
-            $mock->shouldReceive('get')->with(resource_path('docs/main/foo.md'))->andReturn('## Foo title');
-            $mock->shouldReceive('get')->with(resource_path('docs/main/toc.json'))->andReturn($this->toc());
+            $mock->shouldReceive('get')->with(resource_path('docs/0.x/foo.md'))->andReturn('## Foo title');
+            $mock->shouldReceive('get')->with(resource_path('docs/0.x/toc.json'))->andReturn($this->toc());
         });
 
-        $this->get('/docs/main/foo')
+        $this->get('/docs/0.x/foo')
             ->assertSee('<h2>Foo title</h2>', false);
     }
 
@@ -26,14 +26,14 @@ class DocsTest extends TestCase
     public function default_page_redirects_to_main()
     {
         $this->get('/docs')
-            ->assertRedirect('/docs/main/introduction');
+            ->assertRedirect('/docs/0.x/introduction');
     }
 
     /** @test */
     public function pages_without_version_redirect_to_default_version()
     {
         $this->get('/docs/introduction')
-            ->assertRedirect('/docs/main/introduction');
+            ->assertRedirect('/docs/0.x/introduction');
     }
 
     /** @test */
@@ -46,7 +46,7 @@ class DocsTest extends TestCase
     /** @test */
     public function excluded_pages_throw_a_404()
     {
-        $this->get('/docs/main/readme')
+        $this->get('/docs/0.x/readme')
             ->assertStatus(404);
     }
 
