35d4903122470f95a801794e9c8d5d84e4c78fe4
Dries Vints <dries@vints.io>
2021-05-05 20:49:03 +0200

    Implement Meilisearch (#23)
    * Implement Meilisearch
    
    * Update CI
    
    * Create sqlite db
    
    * tweak
    
    * force
    
    * wip
    
    * Fix tests
    
    * env

--- README.md
+++ README.md
@@ -24,9 +24,35 @@ The following tools are required in order to start the installation.
 3. Install the PHP dependencies with `composer install` 
 4. Generate a new app key with `php artisan key:generate`
 5. Install and compile the front-end dependencies with `npm install && npm run dev`
-6. Serve the website locally by running `php artisan serve`
+6. Import all icon sets using `php artisan icons:import`
+7. [Set up Meilisearch (optionally)](#meilisearch)
+8. Serve the website locally by running `php artisan serve`
 
 You can now visit the app in your browser by visiting [http://127.0.0.1:8000](http://127.0.0.1:8000).
+
+### Meilisearch
+
+To get up and running with Meilisearch for the website locally, first make sure you [download and launch Meilisearch](https://docs.meilisearch.com/learn/getting_started/installation.html#download-and-launch).
+
+After that, create an `icons` index using:
+
+```bash
+php artisan scout:index icons
+```
+
+Then, import all icons into the search using:
+
+```bash
+php artisan scout:import "App\\Models\\Icon"
+```
+
+Now all icons from all icon sets are indexed and searchable at [http://127.0.0.1:8000/blade-icons](http://127.0.0.1:8000/blade-icons).
+
+You can clear all icons again by running:
+
+```bash
+php artisan scout:flush "App\\Models\\Icon"
+```
 
 ## Commands
 
@@ -34,4 +60,5 @@ Command | Description
 --- | ---
 **`php artisan test`** | Run the tests
 `php artisan migrate:fresh --seed` | Reset the database
+`php artisan icons:import` | Import all icon sets into Sqlite
 `npm run watch` | Watch for changes in CSS and JS files
--- app/Models/Icon.php
+++ app/Models/Icon.php
@@ -4,13 +4,15 @@ declare(strict_types=1);
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Support\Str;
+use Illuminate\Support\LazyCollection;
+use Laravel\Scout\Searchable;
 
 final class Icon extends Model
 {
+    use Searchable;
+
     /**
      * Indicates if the model should be timestamped.
      *
@@ -30,38 +32,13 @@ final class Icon extends Model
         return $this->belongsTo(IconSet::class, 'icon_set_id');
     }
 
+    public static function relatedIcons(self $icon): LazyCollection
+    {
+        return static::search($icon->keywords)->get();
+    }
+
     public function getRouteKeyName(): string
     {
         return 'name';
-    }
-
-    public static function search(string $search): Collection
-    {
-        $matchesKeywords = function ($query) use ($search) {
-            Str::of($search)->lower()->explode(' ')->filter()->each(function (string $keyword) use ($query) {
-                $query->where('keywords', 'like', "-%{$keyword}%-");
-            });
-
-            $query->limit(500);
-        };
-
-        return self::query()->when($search !== '', $matchesKeywords, function ($query) {
-            $query->inRandomOrder()->limit(72);
-        })->get();
-    }
-
-    public static function relatedIcons(self $icon): Collection
-    {
-        $hasRelatedKeywords = function ($query) use ($icon) {
-            Str::of($icon->keywords)->lower()->explode('-')->filter()->each(function (string $keyword) use ($query) {
-                $query->orWhere('keywords', 'like', "-%{$keyword}%-");
-            });
-        };
-
-        return self::where($hasRelatedKeywords)
-            ->where('id', '!=', $icon->id)
-            ->inRandomOrder()
-            ->limit(20)
-            ->get();
     }
 }
