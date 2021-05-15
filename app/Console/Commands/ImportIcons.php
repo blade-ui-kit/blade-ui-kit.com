04ed572a340614000b3cbdcfa5e30bb6d88f9798
Dries Vints <dries@vints.io>
2021-05-15 22:16:30 +0200

    Pause sync on icon import

--- app/Console/Commands/ImportIcons.php
+++ app/Console/Commands/ImportIcons.php
@@ -30,7 +30,9 @@ final class ImportIcons extends Command
         $this->info('Starting to import icon sets...');
 
         DB::transaction(function () {
-            Icon::query()->delete();
+            Icon::withoutSyncingToSearch(function () {
+                Icon::query()->delete();
+            });
 
             IconSet::each(function (IconSet $iconSet) {
                 $this->parseIcons($iconSet);
@@ -44,21 +46,23 @@ final class ImportIcons extends Command
 
     private function parseIcons(IconSet $iconSet): void
     {
-        $set = $this->sets[$iconSet->name];
+        Icon::withoutSyncingToSearch(function () use ($iconSet) {
+            $set = $this->sets[$iconSet->name];
 
-        foreach ($set['paths'] as $path) {
-            foreach (File::allFiles($path) as $file) {
-                $pathParts = array_filter(explode('/', Str::after($file->getPath(), $path)));
-                $filename = implode('.', array_filter($pathParts + [$file->getFilenameWithoutExtension()]));
+            foreach ($set['paths'] as $path) {
+                foreach (File::allFiles($path) as $file) {
+                    $pathParts = array_filter(explode('/', Str::after($file->getPath(), $path)));
+                    $filename = implode('.', array_filter($pathParts + [$file->getFilenameWithoutExtension()]));
 
-                Icon::create([
-                    'icon_set_id' => $iconSet->id,
-                    'name' => $set['prefix'].'-'.$filename,
-                    'outlined' => $this->isOutlined($filename, $iconSet->outline_rule),
-                    'keywords' => $this->keywords($filename, $iconSet->ignore_rule),
-                ]);
+                    Icon::create([
+                        'icon_set_id' => $iconSet->id,
+                        'name' => $set['prefix'].'-'.$filename,
+                        'outlined' => $this->isOutlined($filename, $iconSet->outline_rule),
+                        'keywords' => $this->keywords($filename, $iconSet->ignore_rule),
+                    ]);
+                }
             }
-        }
+        });
     }
 
     private function keywords(string $string, ?string $rule): string
