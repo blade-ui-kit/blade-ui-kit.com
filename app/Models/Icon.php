a80d1eafae398de6e75a02582f76d67e6de86dca
Adrian Nürnberger <adrian@nuernberger.me>
2021-06-17 18:53:44 +0200

    cs

--- app/Models/Icon.php
+++ app/Models/Icon.php
@@ -4,11 +4,9 @@ declare(strict_types=1);
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Support\Str;
 use Laravel\Scout\Searchable;
 
 final class Icon extends Model
