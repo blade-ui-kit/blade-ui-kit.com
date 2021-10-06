c90e3483e1a224ee385bed3efe520221304595ae
Dries Vints <dries@vints.io>
2021-10-06 16:24:38 +0200

    banners (#67)
    * wip
    
    * wip
    
    * Banners

--- resources/views/blade-icons/index.blade.php
+++ resources/views/blade-icons/index.blade.php
@@ -4,10 +4,18 @@
     @include('welcome.blade-icons')
 
     <div class="mt-16 max-w-screen-2xl mx-auto px-4 sm:px-6 lg:px-8">
-        <div class="max-w-4xl mx-auto text-center">
+        <div class="text-center w-full">
+            <a href="https://www.cloudways.com/en/laravel-hosting?id=994072&data1=bladeUIkit&data2=banner" target="_blank" rel="noopener noreferrer">
+                <img class="w-full max-w-4xl hidden sm:inline-block" src="{{ asset('images/showcase/cloudways-long.jpg') }}" alt="">
+                <img class="inline-block sm:hidden w-full max-w-xs" src="{{ asset('images/showcase/cloudways-small.jpg') }}" alt="">
+            </a>
+        </div>
+
+        <div class="mt-16 max-w-4xl mx-auto text-center">
             <x-h3>
                 Search for an icon
             </x-h3>
+
             <x-p>
                 With {{ App\Models\IconSet::count() }} different icon sets, we probably can find the right one for you.
             </x-p>
