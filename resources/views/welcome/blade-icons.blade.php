181a228fc1c2d0f3d81897a75d839b0ee6329065
Dries Vints <dries@vints.io>
2022-09-09 22:08:43 +0200

    Update

--- resources/views/welcome/blade-icons.blade.php
+++ resources/views/welcome/blade-icons.blade.php
@@ -29,7 +29,7 @@
 
                         <div class="mt-3 sm:mt-0 sm:ml-3">
                             <x-buttons.secondary :href="route('blade-icons').'#search'">
-                                <x-heroicon-s-search class="-ml-1 mr-3 h-5 w-5" />
+                                <x-heroicon-s-magnifying-glass class="-ml-1 mr-3 h-5 w-5" />
                                 Search Icons
                             </x-buttons.secondary>
                         </div>
