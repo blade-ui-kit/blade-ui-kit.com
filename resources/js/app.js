95c4b490c4e5e6fc0dd37918e17c07fd3c04c236
Dries Vints <dries@vints.be>
2024-02-15 09:14:00 +0100

    Update Scout

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,7 +6,7 @@ import hljs from 'highlight.js';
 
 document.addEventListener('DOMContentLoaded', (event) => {
     document.querySelectorAll('pre code').forEach((block) => {
-        hljs.highlightBlock(block);
+        hljs.highlightElement(block);
     });
 });
 
